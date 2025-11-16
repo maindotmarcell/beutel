81cee36b79364fbaed3b321ed151b0eed15a9242
Marcell Munnich <marcellmunnich@pm.me>
2025-11-16 18:21:19 +0100

    chore: setup nativewind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-lg">Welcome to Beutel!</Text>
+      <Text className="text-md">A wallet that makes using Bitcoin easy...</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
