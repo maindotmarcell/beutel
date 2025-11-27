20aaaa8192427c830d426a9a5a5cfbe5a7bbba49
Marcell Munnich <marcellmunnich@pm.me>
2025-11-27 20:16:27 +0100

    refactor: move views and handlers out of app.tsx
    temp

---
+++ components/ViewWrapper.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from "expo-status-bar";
+import { Animated } from "react-native";
+import { BlurView } from "expo-blur";
+import WalletScreen from "../screens/WalletScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import TransactionDetailScreen from "../screens/TransactionDetailScreen";
+import SendScreen from "../screens/SendScreen";
+import { useNavigationStore } from "../store/navigationStore";
+
+const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
+
+export default function ViewWrapper() {
+  const {
+    currentScreen,
+    selectedTransaction,
+    walletTranslateX,
+    walletOpacity,
+    settingsTranslateX,
+    settingsOpacity,
+    transactionDetailTranslateY,
+    transactionDetailOpacity,
+    sendTranslateY,
+    sendOpacity,
+    blurIntensity,
+  } = useNavigationStore();
+
+  return (
+    <>
+      <Animated.View
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          opacity: walletOpacity,
+          transform: [{ translateX: walletTranslateX }],
+        }}
+        pointerEvents={currentScreen === "wallet" ? "auto" : "none"}
+      >
+        <WalletScreen />
+      </Animated.View>
+      <AnimatedBlurView
+        intensity={blurIntensity}
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+        }}
+        pointerEvents="none"
+      />
+      <Animated.View
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          opacity: settingsOpacity,
+          transform: [{ translateX: settingsTranslateX }],
+        }}
+        pointerEvents={currentScreen === "settings" ? "auto" : "none"}
+      >
+        <SettingsScreen />
+      </Animated.View>
+      {selectedTransaction && (
+        <Animated.View
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            opacity: transactionDetailOpacity,
+            transform: [{ translateY: transactionDetailTranslateY }],
+          }}
+          pointerEvents={
+            currentScreen === "transactionDetail" ? "auto" : "none"
+          }
+        >
+          <TransactionDetailScreen />
+        </Animated.View>
+      )}
+      <Animated.View
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          opacity: sendOpacity,
+          transform: [{ translateY: sendTranslateY }],
+        }}
+        pointerEvents={currentScreen === "send" ? "auto" : "none"}
+      >
+        <SendScreen />
+      </Animated.View>
+      <StatusBar style="auto" />
+    </>
+  );
+}
